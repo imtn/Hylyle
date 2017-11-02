19e670c7a8670a3c3aadb2d45df256d52361d653
Niandong Xu <tianxu2009@gmail.com>
2017-11-02 16:28:01 -0400

    added links at top to create and view events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :get_user
   include SessionsHelper
+
+  private
+    def get_user
+      @user = current_user
+    end
 end
